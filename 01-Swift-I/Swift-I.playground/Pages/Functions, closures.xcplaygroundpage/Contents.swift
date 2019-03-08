cf8d8a3050584e438ebc3866a57a5abc2cc67cfa
Jan Schwarz <jan.schwarz@strv.com>
2019-03-08 11:24:11 +0100

    Add examples for topic that weren't explained during the first lecture

--- 01-Swift-I/Swift-I.playground/Pages/Functions, closures.xcplaygroundpage/Contents.swift
+++ 01-Swift-I/Swift-I.playground/Pages/Functions, closures.xcplaygroundpage/Contents.swift
@@ -8,46 +8,95 @@ example(of: "Void functions") {
     func helloWorld() {
         print("Hello world")
     }
-    // Definition with Void
-    // Definition with empty tuple and return
-    // Use functions
+    func helloWorld1() -> Void {
+        print("Hello world 1")
+    }
+    func helloWorld2() -> () {
+        print("Hello world 2")
+        return
+    }
+    
+    helloWorld()
+    helloWorld1()
+    helloWorld2()
 }
 
 //: ### Return value
 example(of: "Function with return value") {
-    // Hello world that returns a value
+    func getHelloWorld() -> String {
+        return "Hello world"
+    }
+    
+    print(getHelloWorld())
 }
 
 //: ### Parameters
 example(of: "Function with parameters") {
-    // Add function with named parameters
-    // Add function without parameter names
-    // Add function with fancy parameter names
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    func anonymousAdd(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+    
+    func fancyAdd(augend a: Int, addend b: Int) -> Int {
+        return a + b
+    }
+    
+    print(add(a: 1, b: 2))
+    print(anonymousAdd(1, 2))
+    print(fancyAdd(augend: 1, addend: 2))
 }
 
 //: ### Default value
 example(of: "Function parameter with default value") {
-    // Add to function with default value
+    func add(number a: Int, to b: Int = 48) -> Int {
+        return a + b
+    }
+    
+    print(add(number: 2, to: 40))
+    print(add(number: 2))
 }
 
 //: ## Closures
 //: ### Function type
 example(of: "Function type") {
-    // Add function
-    // Assign reference to variable
-    // Type of
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    let addFunction = add
+    
+    print(type(of: addFunction))
 }
 
 //: ### Closure declaration
 example(of: "Closure declaration") {
-    // Add closure
+    let add: (Int, Int) -> Int = { a, b in
+        return a + b
+    }
+    
+    print(add(1, 2))
 }
 
 //: ### Closure as a function parameter
 example(of: "Closure as a function parameter") {
-    // Combine with a closure
-    // Additon closure
-    // Subtraction closure
+    func combine(a: Int, b: Int, with block: (Int, Int) -> Int) -> Int {
+        return block(a, b)
+    }
+    
+    let addition = combine(a: 1, b: 2, with: { a, b in
+        return a + b
+    })
+    print(addition)
+    
+    let subtract: (Int, Int) -> Int = { a, b in
+        return a - b
+    }
+    
+    let subtraction = combine(a: 1, b: 2, with: subtract)
+    print(subtraction)
 }
 
 //: [Next](@next)
